185b5b462e824440ac398546041f699fcac1cedb
Nate C <114708534+Harank-sucks-at-coding@users.noreply.github.com>
2023-01-21 14:56:10 -0500

    hk drivetrsin
    hola

--- src/main/java/frc/robot/commands/JoystickDrive.java
+++ src/main/java/frc/robot/commands/JoystickDrive.java
@@ -4,12 +4,20 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveSubsystem;
 
 public class JoystickDrive extends CommandBase {
-  /** Creates a new JoystickDrive. */
-  public JoystickDrive() {
-    // Use addRequirements() here to declare subsystem dependencies.
+ 
+  private final DriveSubsystem driveSubsystem;
+    private final static XboxController driverController = RobotContainer.driverController;
+
+
+  public JoystickDrive(DriveSubsystem drivetrain) {
+    driveSubsystem = drivetrain;
+    addRequirements(driveSubsystem);
   }
 
   // Called when the command is initially scheduled.
